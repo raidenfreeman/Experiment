071f38c142a2b3b6a4a35125a191be68dcd6b71c
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2020-08-03 23:57:33 +0200

    Update to Unity 2020
    Updated DoTween, Probuilder, TextMesh Pro
    Removed ENet, Wakatime, PowerUI, Standard Assets

--- Assets/NetworkingService.cs
+++ Assets/NetworkingService.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using ENet;
+// using ENet;
 
 public class NetworkingService : MonoBehaviour {
 
@@ -9,94 +9,94 @@ public class NetworkingService : MonoBehaviour {
     private readonly int maxClients = 4;
     public void StartServer()
     {
-        ENet.Library.Initialize();
+        // ENet.Library.Initialize();
         
-        using (Host server = new Host())
-        {
-            Address address = new Address();
+        // using (Host server = new Host())
+        // {
+        //     Address address = new Address();
 
-            address.Port = port;
-            server.Create(address, maxClients);
-            ENet.Event netEvent;
+        //     address.Port = port;
+        //     server.Create(address, maxClients);
+        //     ENet.Event netEvent;
 
-            while (true)
-            {
-                server.Service(15, out netEvent);
+        //     while (true)
+        //     {
+        //         server.Service(15, out netEvent);
 
-                switch (netEvent.Type)
-                {
-                    case ENet.EventType.None:
-                        break;
+        //         switch (netEvent.Type)
+        //         {
+        //             case ENet.EventType.None:
+        //                 break;
 
-                    case ENet.EventType.Connect:
-                        Debug.Log("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                        break;
+        //             case ENet.EventType.Connect:
+        //                 Debug.Log("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+        //                 break;
 
-                    case ENet.EventType.Disconnect:
-                        Debug.Log("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                        break;
+        //             case ENet.EventType.Disconnect:
+        //                 Debug.Log("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+        //                 break;
 
-                    case ENet.EventType.Timeout:
-                        Debug.Log("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
-                        break;
+        //             case ENet.EventType.Timeout:
+        //                 Debug.Log("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+        //                 break;
 
-                    case ENet.EventType.Receive:
-                        Debug.Log("Packet received from - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP + ", Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
-                        netEvent.Packet.Dispose();
-                        break;
-                }
-            }
+        //             case ENet.EventType.Receive:
+        //                 Debug.Log("Packet received from - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP + ", Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+        //                 netEvent.Packet.Dispose();
+        //                 break;
+        //         }
+        //     }
 
-            server.Flush();
-        }
+        //     server.Flush();
+        // }
     }
 
     public void StartClient(string ip)
     {
-        using (Host client = new Host())
-        {
-            Address address = new Address();
+        // using (Host client = new Host())
+        // {
+        //     Address address = new Address();
 
-            address.SetHost(ip);
-            address.Port = port;
-            client.Create();
+        //     address.SetHost(ip);
+        //     address.Port = port;
+        //     client.Create();
 
-            Peer peer = client.Connect(address);
-            ENet.Event netEvent;
-            while (true)
-            {
-                client.Service(15, out netEvent);
+        //     Peer peer = client.Connect(address);
+        //     ENet.Event netEvent;
+        //     while (true)
+        //     {
+        //         client.Service(15, out netEvent);
 
-                switch (netEvent.Type)
-                {
-                    case ENet.EventType.None:
-                        break;
+        //         switch (netEvent.Type)
+        //         {
+        //             case ENet.EventType.None:
+        //                 break;
 
-                    case ENet.EventType.Connect:
-                        Debug.Log("Client connected to server - ID: " + peer.ID);
-                        break;
+        //             case ENet.EventType.Connect:
+        //                 Debug.Log("Client connected to server - ID: " + peer.ID);
+        //                 break;
 
-                    case ENet.EventType.Disconnect:
-                        Debug.Log("Client disconnected from server");
-                        break;
+        //             case ENet.EventType.Disconnect:
+        //                 Debug.Log("Client disconnected from server");
+        //                 break;
 
-                    case ENet.EventType.Timeout:
-                        Debug.Log("Client connection timeout");
-                        break;
+        //             case ENet.EventType.Timeout:
+        //                 Debug.Log("Client connection timeout");
+        //                 break;
 
-                    case ENet.EventType.Receive:
-                        Debug.Log("Packet received from server - Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
-                        netEvent.Packet.Dispose();
-                        byte[] buffer = new byte[1024];
+        //             case ENet.EventType.Receive:
+        //                 Debug.Log("Packet received from server - Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+        //                 netEvent.Packet.Dispose();
+        //                 byte[] buffer = new byte[1024];
 
-                        netEvent.Packet.CopyTo(buffer);
-                        // Cast buffer to .......
-                        break;
-                }
-            }
+        //                 netEvent.Packet.CopyTo(buffer);
+        //                 // Cast buffer to .......
+        //                 break;
+        //         }
+        //     }
 
-            client.Flush();
-        }
+            // client.Flush();
+        // }
     }
 
     
