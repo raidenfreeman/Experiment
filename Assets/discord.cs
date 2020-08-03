ade3da71f438970aa06d041786d0e0ad2ad93802
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2020-08-04 00:44:29 +0200

    Discord  integration running

---
+++ Assets/discord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class discord : MonoBehaviour
+{
+    public Discord.Discord discordClient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        const long CLIENT_ID = 739944939894472811;
+        discordClient = new Discord.Discord(CLIENT_ID, (System.UInt64)Discord.CreateFlags.Default);
+
+        var activityManager = discordClient.GetActivityManager();
+        var activity = new Discord.Activity
+        {
+            State = "Still Testing",
+            Details = "Bigger Test"
+        };
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Discord.Result.Ok)
+            {
+                Debug.LogError("Everything is fine!");
+            }
+
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        discordClient.RunCallbacks();
+    }
+}
