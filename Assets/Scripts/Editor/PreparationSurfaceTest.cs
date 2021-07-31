6c112413dee69a16f8aad33df135bb74c855e8ca
Konstantinos Gkinis <raidenfreeman@hotmail.com>
2021-07-31 11:35:51 +0200

    Duplicate test in new namespace

--- Assets/Scripts/Editor/PreparationSurfaceTest.cs
+++ Assets/Scripts/Editor/PreparationSurfaceTest.cs
@@ -3,67 +3,93 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 using System.Reflection;
-using UnityBridge;
 
-public class PreparationSurfaceTest
+namespace CoreSource
 {
-    [Test]
-    public void TryInteract_NoItemOnSurface_ReturnFalse()
+    public class PreparationSurfaceTest
     {
-        var surface = new GameObject().AddComponent<PreparationSurface>();
-        //var placementSurface = new GameObject().AddComponent<PlacementSurface>();
-        //typeof(PreparationSurface).GetField("placementSurface", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(surface, placementSurface);
-        Assert.IsFalse(surface.TryInteract());
+        [Test]
+        public void TryInteract_NoItemOnSurface_ReturnFalse()
+        {
+            var surface = new PreparationSurface();
+            //var placementSurface = new GameObject().AddComponent<PlacementSurface>();
+            //typeof(PreparationSurface).GetField("placementSurface", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(surface, placementSurface);
+            Assert.IsFalse(surface.TryInteract());
+        }
     }
 
-    [Test]
-    public void TryInteract_NotInteractibleItemOnSurface_ReturnFalse()
+    public class PreparationSurface
     {
-        var surface = new GameObject().AddComponent<PreparationSurface>();
-        //var placementSurface = new GameObject().AddComponent<PlacementSurface>();
-        var pot = new GameObject().AddComponent<Pot>();
-        typeof(PreparationSurface)
-            .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .SetValue(surface, pot);
-        Assert.IsFalse(surface.TryInteract());
+        public bool TryInteract()
+        {
+            return false;
+        }
     }
+}
 
-    [Test]
-    public void TryInteract_NotPreparableItem_ReturnFalse()
-    {
-        var surface = new GameObject().AddComponent<PreparationSurface>();
-        var dish = new GameObject().AddComponent<Dish>();
-        typeof(PreparationSurface)
-            .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .SetValue(surface, dish);
-        Assert.IsFalse(surface.TryInteract());
-    }
 
-    [Test]
-    public void TryInteract_InteractibleItemOnSurface_ReturnTrue()
+namespace UnityBridge
+{
+    public class PreparationSurfaceTest
     {
-        var surface = new GameObject().AddComponent<PreparationSurface>();
-        var tomato = new GameObject().AddComponent<FoodIngredient>();
-        typeof(PreparationSurface)
-            .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .SetValue(surface, tomato);
-        Assert.IsTrue(surface.TryInteract());
-    }
+        [Test]
+        public void TryInteract_NoItemOnSurface_ReturnFalse()
+        {
+            var surface = new GameObject().AddComponent<PreparationSurface>();
+            //var placementSurface = new GameObject().AddComponent<PlacementSurface>();
+            //typeof(PreparationSurface).GetField("placementSurface", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(surface, placementSurface);
+            Assert.IsFalse(surface.TryInteract());
+        }
 
-    [UnityTest]
-    public IEnumerator TryInteract_InteractibleItemOnSurface_CompletionIncreased()
-    {
-        var surface = new GameObject().AddComponent<PreparationSurface>();
-        var tomato = new GameObject().AddComponent<FoodIngredient>();
-        typeof(PreparationSurface)
-            .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-            .SetValue(surface, tomato);
-        var startingPercentage = tomato.completionPercentage;
-        yield return null;
-        surface.TryInteract();
-        var timePrepared = typeof(FoodIngredient)
-            .GetField("timeSpentPreparing", BindingFlags.NonPublic | BindingFlags.Instance)
-            .GetValue(tomato);
-        Assert.AreEqual(Time.deltaTime, timePrepared);
+        [Test]
+        public void TryInteract_NotInteractibleItemOnSurface_ReturnFalse()
+        {
+            var surface = new GameObject().AddComponent<PreparationSurface>();
+            //var placementSurface = new GameObject().AddComponent<PlacementSurface>();
+            var pot = new GameObject().AddComponent<Pot>();
+            typeof(PreparationSurface)
+                .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(surface, pot);
+            Assert.IsFalse(surface.TryInteract());
+        }
+
+        [Test]
+        public void TryInteract_NotPreparableItem_ReturnFalse()
+        {
+            var surface = new GameObject().AddComponent<PreparationSurface>();
+            var dish = new GameObject().AddComponent<Dish>();
+            typeof(PreparationSurface)
+                .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(surface, dish);
+            Assert.IsFalse(surface.TryInteract());
+        }
+
+        [Test]
+        public void TryInteract_InteractibleItemOnSurface_ReturnTrue()
+        {
+            var surface = new GameObject().AddComponent<PreparationSurface>();
+            var tomato = new GameObject().AddComponent<FoodIngredient>();
+            typeof(PreparationSurface)
+                .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(surface, tomato);
+            Assert.IsTrue(surface.TryInteract());
+        }
+
+        [UnityTest]
+        public IEnumerator TryInteract_InteractibleItemOnSurface_CompletionIncreased()
+        {
+            var surface = new GameObject().AddComponent<PreparationSurface>();
+            var tomato = new GameObject().AddComponent<FoodIngredient>();
+            typeof(PreparationSurface)
+                .GetProperty("placedItem", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(surface, tomato);
+            var startingPercentage = tomato.completionPercentage;
+            yield return null;
+            surface.TryInteract();
+            var timePrepared = typeof(FoodIngredient)
+                .GetField("timeSpentPreparing", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(tomato);
+            Assert.AreEqual(Time.deltaTime, timePrepared);
+        }
     }
 }
